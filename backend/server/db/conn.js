69ab5d91b5875fc3f122176e324fc51b037f37c7
Ankit Basare <ankitbasare@gmail.com>
2023-04-04 13:12:19 +0100

    Replacing mongodb with mongoose

--- backend/server/db/conn.js
+++ backend/server/db/conn.js
@@ -1,28 +1,14 @@
-const { MongoClient } = require("mongodb");
-const Db = process.env.MONGO_DB_URL;
-console.log(Db);
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
- 
-var _db;
- 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        console.log("Successfully connected to MongoDB."); 
-      }
-      if(err){
-        console.log("ERROR Received: ",err);
-      }
-      return callback(err);
-    });
-  },
-  getDb: function () {
-    return _db;
-  },
-};
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+  console.log("MONGO_DB_URL: " + process.env.MONGO_DB_URL);  
+  mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+      console.log("Mongoose Connected");
+
+  }).catch((error) => {
+      console.log(error);
+  });
+}
+
+module.exports = connectDatabase;
