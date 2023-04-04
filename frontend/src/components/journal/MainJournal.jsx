238573600bd7b1ec853f7441d93dee21b9586da1
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2023-04-04 21:32:33 +0100

    major frontend changes

--- frontend/src/components/journal/MainJournal.jsx
+++ frontend/src/components/journal/MainJournal.jsx
@@ -32,7 +32,7 @@ function MainJournal() {
             <button
             onClick={() => handleSubmission()} 
             type="button" 
-            class="w-1/5 text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+            className="w-1/5 text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
                 Submit
             </button>
         </div>
