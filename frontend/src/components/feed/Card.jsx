bbaf02021da04fb418706778012a8f24855b168e
Brian Pereira <brianpereira2123@gmail.com>
2023-04-04 17:57:55 +0100

    changed one to image and centered cards

--- frontend/src/components/feed/Card.jsx
+++ frontend/src/components/feed/Card.jsx
@@ -1,14 +1,24 @@
-import { Card } from 'flowbite-react';
+import { Card , CardMedia } from 'flowbite-react';
+import AcceptGroup from "./AcceptGroup";
 
 function MyCard(props) {
+
+    const selection = (emotion) => {
+        // Fill over the code for handling the next pages
+        console.log("EMOTION: ",emotion);
+
+    }
+
     return (
-        <div class="flex items-center bg-indigo-100 w-screen">
-            <div class="container ml-auto mr-auto flex flex-wrap items-start">
 
+        
+        <div class="flex justify-center items-center bg-indigo-100 w-screen bg-transparent">
 
-                <div className="max-w-sm">
-                    <Card imgSrc={props.data.img}>
-                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            
+            <div class="container flex justify-center">
+                <div className="max-w-sm flex flex-col items-center">
+                    <Card style={{ height: '27rem' }}>
+                        {/* <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             {props.data.title}
                         </h5>
 
@@ -18,13 +28,17 @@ function MyCard(props) {
                         <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Read more
                             <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </a>
+                        </a> */}
+                        <img src={props.data.img} ></img>
                     </Card>
+                    <div class="mt-4">
+                    <AcceptGroup selection = {selection} style={{ marginTop: '1rem' }}/>
+                    </div>
                 </div>
             </div>
 
-            <div class="container ml-auto mr-auto flex flex-wrap items-start">
-                <div className="max-w-sm">
+            <div class="container flex justify-center">
+                <div className="max-w-sm flex flex-col items-center">
                     <Card imgSrc={props.data.img}>
                         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             {props.data.title}
@@ -38,11 +52,14 @@ function MyCard(props) {
                             <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </a>
                     </Card>
+                    <div class="mt-4">
+                    <AcceptGroup selection = {selection} style={{ marginTop: '1rem' }}/>
+                    </div>
                 </div>
             </div>
-            <div class="container ml-auto mr-auto flex flex-wrap items-start">
+            <div class="container flex justify-center">
 
-                <div className="max-w-sm">
+                <div className="max-w-sm flex flex-col items-center">
                     <Card imgSrc={props.data.img}>
                         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             {props.data.title}
@@ -56,6 +73,9 @@ function MyCard(props) {
                             <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </a>
                     </Card>
+                    <div class="mt-4">
+                    <AcceptGroup selection = {selection} style={{ marginTop: '1rem' }}/>
+                    </div>
 
                 </div>
             </div>
