151834a14d91a130a49b7bb214a86f9e5fc256a5
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2023-04-04 13:09:32 +0100

    Frontend components for base login (#1)
    * Adding in the frontend components
    
    * Centering elements for root app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
 import Landing from "./components/user/Landing";
+import MainFeed from "./components/feed/MainFeed";
+import { useState } from "react";
 // Handle data for login user
+// TODO CHANGE PROPS TO DATA RECEIVED FROM API
 function App() {
+  const [currentState,setCurrentState] = useState("land");
+  const [currentEmotion, setCurrentEmotion] = useState("");
+  const updateState = (state) => {
+    setCurrentState(state);
+  }
+  const updateCurrentEmotion = (emotion) => {
+    setCurrentEmotion(emotion);
+  }
   return (
-   <Landing
-   name = "DEMO"/>
+  <div className = 'h-screen bg-gradient-to-l from-purple-600 via-pink-300 to-blue-400 flex items-center justify-center h-screen'>
+   {currentState === 'land' && <Landing
+   name = "DEMO"
+   updateStateHandle = {updateState}
+   updateCurrentEmotion = {updateCurrentEmotion}/>
+  }
+  {currentState === 'feed' && <MainFeed
+  currentEmotion = {currentEmotion}
+  updateStateHandle = {updateState}/>
+  }
+   </div>
   );
 }
 export default App;
--- frontend/src/components/user/Landing.jsx
+++ frontend/src/components/user/Landing.jsx
@@ -1,8 +1,23 @@
-function Landing(props){
+import Emoji from "./Emoji";
+function Landing(props) {
+
+    const clickHandler = (emotion) => {
+        // Fill over the code for handling the next pages
+        console.log("EMOTION: ",emotion);
+        // Swap the component to be loaded
+        props.updateStateHandle("feed");
+    }
     return (
-        <div className="landing-main">
-            {(props.name || "DEMO USER")}
-        </div>
+        <div className="container mx-auto px-6 flex-col">
+            <h2 className="text-4xl font-bold mb-2 text-white flex-auto ml-auto mr-auto">
+                {`Hey, ${props.name}`}
+            </h2>
+            <h3 className="text-2xl mb-8 text-gray-200">
+                How are you feeling today ? 
+            </h3>
+            <Emoji
+            clickHandler = {clickHandler}/>
+        </div>        
     )
 }
 export default Landing;
