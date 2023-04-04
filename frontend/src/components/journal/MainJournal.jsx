f0b5ed87abab98c69b7dd1164725c039c7f2a02a
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2023-04-04 16:32:03 +0100

    Minor bug fixes

--- frontend/src/components/journal/MainJournal.jsx
+++ frontend/src/components/journal/MainJournal.jsx
@@ -21,9 +21,10 @@ function MainJournal() {
                 Journal Entry
             </h2>
             <textarea
+                spellCheck="true"
                 id="messageArea"
-                rows="4"
-                className="block p-2.5 w-3/4 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                rows="20"
+                className="shadow-lg shadow-slate-300 rounded-lg block p-2.5 w-3/4 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Write your thoughts here...">
             </textarea>
             <Emoji
